2b239d74c832f7a0477aae1432332975c9768272
ray <enciso.leo@gmail.com>
2024-12-13 13:21:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import grpc
+import protos.contract_pb2_grpc as pb2_grpc
+import protos.contract_pb2 as pb2
+
+
+class GrpcClient(object):
+    """
+    Client for gRPC functionality
+    """
+
+    def __init__(self, host):
+        self.host = host  # Use the CodeSandbox URL here
+
+        # instantiate a channel
+        self.channel = grpc.insecure_channel(self.host)
+
+        # bind the client and the server
+        self.stub = pb2_grpc.EquisDeStub(self.channel)
+
+    def get_url(self, message):
+        """
+        Client function to call the rpc for GetServerResponse
+        """
+        message = pb2.MessageRequest(message=message)
+        print(f"{message}")
+        return self.stub.Ping(message)
+
+
+if __name__ == '__main__':
+    # Replace with the actual CodeSandbox URL
+    client = GrpcClient('https://2g4j42-50051.csb.app/')
+    result = client.get_url(message="Hello Server you there?")
+    print(f"{result}")
